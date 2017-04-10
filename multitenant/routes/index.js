e73089c466b903722fc8d7b91163de7c87886862
neilthaker07 <neilthaker07@gmail.com>
2017-04-10 12:48:51 -0700

    Node js implementation started.

--- multitenant/routes/index.js
+++ multitenant/routes/index.js
@@ -2,8 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index');
+});
+
+router.get('/newgrader', function (req, res, next) {
+  res.render('newgrader');
+});
+
+router.post('/signup', function(req, res, next) {
+    res.redirect('/newgrader');
+});
+
+router.get('/grader/:username', function (req, res, next) {
+  res.render('grader',{grader: req.params.username});
+});
+
+router.post('/login', function(req, res, next) {
+  var username = req.body.username;
+  res.redirect('/grader/'+username);
 });
 
 module.exports = router;
